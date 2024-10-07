3baed5a9beb954404d9607cb1e21526be788e40b
tchen <chen2015tong@gmail.com>
2024-10-07 17:30:49 +0200

    add resources controller

---
+++ spring-boot/spring-boot-application/src/main/java/com/springboot/main/controller/ResourcesController.java
@@ -0,0 +1,24 @@
+package com.springboot.main.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+@RestController
+public class ResourcesController {
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @GetMapping("/resources")
+    public ResponseEntity<String> resource() throws IOException {
+        String content = this.resourceLoader.getResource("classpath:resource.xml")
+                .getContentAsString(Charset.defaultCharset());
+        return ResponseEntity.ok(content);
+    }
+}
