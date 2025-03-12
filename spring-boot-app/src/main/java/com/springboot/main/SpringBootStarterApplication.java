63148befbfc93cc22bf932caee51e866617fb476
tchen <chen2015tong@gmail.com>
2025-03-12 12:04:36 +0100

    refresh project

--- spring-boot-app/src/main/java/com/springboot/main/SpringBootStarterApplication.java
+++ spring-boot-app/src/main/java/com/springboot/main/SpringBootStarterApplication.java
@@ -7,17 +7,6 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 import java.io.File;
 
-// TODO. Spring Boot启动属性配置
-// 1. 启动类型不能放置在空包路径下(default package)
-// 2. 默认扫描范围@SpringBootApplication注解类型的package下所有包
-//    扫描所有标注@Repository, @Service, @Component, @Controller...的类型
-// 3. 可以移除对于特定名称的扫描，或者自定义扫描的包名scanBasePackages
-// @ComponentScan(excludeFilters = {
-//      @Filter(type = FilterType.CUSTOM,classes = {TypeExcludeFilter.class}),
-//      @Filter(type = FilterType.CUSTOM,classes = {AutoConfigurationExcludeFilter.class}
-//    )}
-// @EntityScan("com.spring.data.jpa.entity")
-// @EnableJpaRepositories("com.spring.data.jpa.repositories")
 
 @SpringBootApplication
 public class SpringBootStarterApplication {
