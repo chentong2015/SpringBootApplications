ea0e5d89075f58b2ff3c67f0552d5a6eec29a5b3
tchen <chen2015tong@gmail.com>
2025-04-10 15:40:05 +0200

    add spring boot starter health

--- spring-boot-app/src/main/java/com/springboot/main/SpringBootHealthIndicator.java
+++ spring-boot-app/src/main/java/com/springboot/main/SpringBootHealthIndicator.java
@@ -12,12 +12,13 @@ public class SpringBootHealthIndicator implements HealthIndicator {
 
     @Override
     public Health health() {
+        // 获取C盘剩余的Drive Space存储空间大小
         // Path used to compute available disk space
         File path = new File(".");
-        long diskUsableInBytes = path.getUsableSpace();
+        long diskUsableInBytes = path.getUsableSpace() / (1024 * 1024);
 
         // Check if space is more than 100M
-        boolean hasEnoughSpace = diskUsableInBytes >= 100 * 1024 * 1024;
+        boolean hasEnoughSpace = diskUsableInBytes >= 100;
 
         // UP means there is enough usable memory.
         Status status = hasEnoughSpace ? Status.UP : Status.DOWN;
@@ -25,8 +26,8 @@ public class SpringBootHealthIndicator implements HealthIndicator {
         // In addition to reporting the status,
         // we can attach additional key-value details using the withDetail(key, value)
         return Health.status(status)
-                .withDetail("usable memory", diskUsableInBytes)
-                .withDetail("threshold", 100 * 1024 * 1024)
+                .withDetail("Usable memory (M)", diskUsableInBytes)
+                .withDetail("Threshold (M)", 100)
                 .build();
     }
 }
--- spring-boot-app/src/main/java/com/springboot/main/SpringBootStarterApplication.java
+++ spring-boot-app/src/main/java/com/springboot/main/SpringBootStarterApplication.java
@@ -1,12 +1,24 @@
 package com.springboot.main;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.system.ApplicationHome;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.io.File;
 
+// TODO. Spring Boot启动属性配置
+// 1. 启动类型不能放置在空包路径下(default package)
+// 2. 默认扫描范围@SpringBootApplication注解类型的package下所有包
+//    扫描所有标注@Repository, @Service, @Component, @Controller...的类型
+// 3. 可以移除对于特定名称的扫描，或者自定义扫描的包名scanBasePackages
+// @ComponentScan(excludeFilters = {
+//     @Filter(type = FilterType.CUSTOM,classes = {TypeExcludeFilter.class}),
+//     @Filter(type = FilterType.CUSTOM,classes = {AutoConfigurationExcludeFilter.class}
+//   )}
+// @EntityScan("com.spring.data.jpa.entity")
+// @EnableJpaRepositories("com.spring.data.jpa.repositories")
 @SpringBootApplication
 public class SpringBootStarterApplication {
 
@@ -14,11 +26,16 @@ public class SpringBootStarterApplication {
         // Stop showing slf4j internal source code message
         System.setProperty("slf4j.internal.verbosity", "WARN");
 
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootStarterApplication.class, args);
+        ConfigurableApplicationContext applicationContext =
+                SpringApplication.run(SpringBootStarterApplication.class, args);
         System.out.println(applicationContext.isActive());
 
         ApplicationHome home = new ApplicationHome(SpringBootStarterApplication.class);
         File folder = home.getDir();          // returns the folder where the jar is.
         File folderSource = home.getSource(); // returns the jar absolute filepath.
+
+        SpringBootHealthIndicator healthIndicator = applicationContext.getBean(SpringBootHealthIndicator.class);
+        Health health = healthIndicator.health();
+        System.out.println(health.getStatus().getCode()); // UP or DOWN
     }
 }
