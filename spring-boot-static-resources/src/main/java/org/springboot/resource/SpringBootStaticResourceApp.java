12c09718ab7878aa0fd67d794df34412ea1b5518
chent5 <chen2015tong@gmail.com>
2025-06-25 18:07:00 +0200

    add spring boot static resources module

---
+++ spring-boot-static-resources/src/main/java/org/springboot/resource/SpringBootStaticResourceApp.java
@@ -0,0 +1,12 @@
+package org.springboot.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStaticResourceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStaticResourceApp.class, args);
+    }
+}
