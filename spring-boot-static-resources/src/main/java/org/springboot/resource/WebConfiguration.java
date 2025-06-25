9a0e4d28dfee49c86bd3db9703e7a3517b1ccee1
chent5 <chen2015tong@gmail.com>
2025-06-25 18:52:13 +0200

    update spring boot static resource cache

---
+++ spring-boot-static-resources/src/main/java/org/springboot/resource/WebConfiguration.java
@@ -0,0 +1,24 @@
+package org.springboot.resource;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.Duration;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    // TODO. 配置静态资源的缓存策略，避免后端频繁的返回数据(无更新时)
+    // The resources are served with 7 days future expiration
+    // to ensure maximum use of the browser cache and a reduction in HTTP requests made by the browser.
+    // The Last-Modified information is deduced from Resource#lastModified
+    // so that HTTP conditional requests are supported with "Last-Modified" headers.
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/public", "classpath:/static/")
+                .setCacheControl(CacheControl.maxAge(Duration.ofDays(7)));
+    }
+}
--- spring-boot-static-resources/src/main/java/org/springboot/resource/WebStaticResourceApp.java
+++ spring-boot-static-resources/src/main/java/org/springboot/resource/WebStaticResourceApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootStaticResourceApp {
+public class WebStaticResourceApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootStaticResourceApp.class, args);
+        SpringApplication.run(WebStaticResourceApp.class, args);
     }
 }
