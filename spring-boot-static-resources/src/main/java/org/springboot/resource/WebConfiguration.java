168dba88ba824266efa1399d13f487f414a23162
chent5 <chen2015tong@gmail.com>
2025-06-26 11:08:04 +0200

    finish spring boot static resource

--- spring-boot-static-resources/src/main/java/org/springboot/resource/WebConfiguration.java
+++ spring-boot-static-resources/src/main/java/org/springboot/resource/WebConfiguration.java
@@ -10,8 +10,13 @@ import java.time.Duration;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
-    // TODO. 配置静态资源的缓存策略, 避免后端频繁的返回数据(无更新时)
-    //  当用户清除浏览器缓存后, 请求时必须重新返回数据到客户端
+    // 所有访问/resources/**路径的请求都能从Locations中找到静态文件
+    // http://localhost:8080/v1/resources/Hello.html
+    //
+    // 支持设置多个静态资源路径位置
+    // .addResourceLocations("/resources/","classpath:/other-resources/");
+    //
+    // 配置静态资源的缓存策略, 避免后端频繁的返回数据(无更新时)
     // The resources are served with 7 days future expiration
     // to ensure maximum use of the browser cache and a reduction in HTTP requests made by the browser.
     // The Last-Modified information is deduced from Resource#lastModified
